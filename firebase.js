ec9c1742e534d87d4461e4072cd0a88a4f28690e
saoadept41-beep <saoadept41@gmail.com>
2026-02-06 15:28:54 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -3,18 +3,17 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAwh_V0jTicgXh7enrzbMXM4_78JgYY9UY",
-  authDomain: "k-net-83161.firebaseapp.com",
-  databaseURL: "https://k-net-83161-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "k-net-83161",
-  storageBucket: "k-net-83161.firebasestorage.app",
-  messagingSenderId: "1093304648278",
-  appId: "1:1093304648278:web:5a325e951273bb9f04b1f7"
+  apiKey: "ТВОЙ_API_KEY",
+  authDomain: "ТВОЙ_PROJECT.firebaseapp.com",
+  databaseURL: "https://ТВОЙ_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "ТВОЙ_PROJECT",
+  storageBucket: "ТВОЙ_PROJECT.appspot.com",
+  messagingSenderId: "XXXX",
+  appId: "XXXX"
 };
 
-const app = initializeApp(firebaseConfig);
-
+export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getDatabase(app);
 
-console.log("🔥 Firebase initialized");
+
