f98d9ed02118b368a16982b82eea27dc1f4ce578
saoadept41-beep <saoadept41@gmail.com>
2026-02-06 15:30:01 +0100

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,19 +1,24 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+mport { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getDatabase } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-database.js";
 
 const firebaseConfig = {
-  apiKey: "ТВОЙ_API_KEY",
-  authDomain: "ТВОЙ_PROJECT.firebaseapp.com",
-  databaseURL: "https://ТВОЙ_PROJECT-default-rtdb.europe-west1.firebasedatabase.app",
-  projectId: "ТВОЙ_PROJECT",
-  storageBucket: "ТВОЙ_PROJECT.appspot.com",
-  messagingSenderId: "XXXX",
-  appId: "XXXX"
+  apiKey: "AIzaSyAwh_V0jTicgXh7enrzbMXM4_78JgYY9UY",
+  authDomain: "k-net-83161.firebaseapp.com",
+  databaseURL: "https://k-net-83161-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "k-net-83161",
+  storageBucket: "k-net-83161.firebasestorage.app",
+  messagingSenderId: "1093304648278",
+  appId: "1:1093304648278:web:5a325e951273bb9f04b1f7"
 };
 
-export const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
+
 export const auth = getAuth(app);
 export const db = getDatabase(app);
+
+console.log("🔥 Firebase initialized");
+
+
 
 
