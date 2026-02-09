d3ebd6a3e8f5124269a37c1b0c566aef238b2b54
saoadept41-beep <saoadept41@gmail.com>
2026-02-09 11:01:45 +0100

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -4,29 +4,35 @@ import {
   createUserWithEmailAndPassword
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-document.addEventListener("DOMContentLoaded", () => {
+const sphere = document.getElementById("sphere");
+const main = document.getElementById("main-screen");
+const authPanel = document.getElementById("auth-panel");
 
-  const sphere = document.getElementById("sphere");
-  const panel = document.getElementById("auth-panel");
+sphere.onclick = () => {
+  main.classList.add("hidden");
+  authPanel.classList.add("show");
+};
 
-  sphere.onclick = () => {
-    panel.classList.add("show");
-  };
+loginForm.onsubmit = e => {
+  e.preventDefault();
+  signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
+    .then(() => location.href = "dashboard.html")
+    .catch(err => error.textContent = err.message);
+};
 
-  loginForm.onsubmit = e => {
-    e.preventDefault();
-    signInWithEmailAndPassword(auth, loginEmail.value, loginPassword.value)
-      .then(() => location.href = "dashboard.html")
-      .catch(err => errorMessage.textContent = err.message);
-  };
+registerForm.onsubmit = e => {
+  e.preventDefault();
+  createUserWithEmailAndPassword(auth, regEmail.value, regPassword.value)
+    .then(() => location.href = "dashboard.html")
+    .catch(err => error.textContent = err.message);
+};
 
-  registerForm.onsubmit = e => {
-    e.preventDefault();
-    createUserWithEmailAndPassword(auth, regEmail.value, regPassword.value)
-      .then(() => location.href = "dashboard.html")
-      .catch(err => errorMessage.textContent = err.message);
-  };
+loginTab.onclick = () => {
+  loginForm.classList.remove("hidden");
+  registerForm.classList.add("hidden");
+};
 
-});
-
-
+registerTab.onclick = () => {
+  registerForm.classList.remove("hidden");
+  loginForm.classList.add("hidden");
+};
