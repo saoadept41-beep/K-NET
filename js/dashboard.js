625e0580d208dcb5e82c3b5a1e5bb8f017d95555
saoadept41-beep <saoadept41@gmail.com>
2026-02-09 09:57:43 +0100

    Create dashboard.js

---
+++ js/dashboard.js
@@ -0,0 +1,38 @@
+import { auth, db } from "./firebase.js";
+import {
+  collection, addDoc, query, orderBy, onSnapshot
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+let currentChat = "global";
+
+document.querySelectorAll(".sidebar button").forEach(btn=>{
+  btn.onclick=()=>loadChat(btn.dataset.chat);
+});
+
+function loadChat(name){
+  currentChat = name;
+  const q = query(
+    collection(db,"chats",name,"messages"),
+    orderBy("time")
+  );
+  onSnapshot(q,snap=>{
+    messages.innerHTML="";
+    snap.forEach(d=>{
+      messages.innerHTML += `<div>${d.data().text}</div>`;
+    });
+  });
+}
+
+sendForm.onsubmit = async e=>{
+  e.preventDefault();
+  await addDoc(collection(db,"chats",currentChat,"messages"),{
+    text: text.value,
+    time: Date.now()
+  });
+  text.value="";
+};
+
+loadChat("global");
+
+
+
